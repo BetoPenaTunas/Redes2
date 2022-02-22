f5dfc5cdc7e0dee3cd4e405802e955414557386f
BetoPenaTunas <94942105+BetoPenaTunas@users.noreply.github.com>
2022-02-22 07:44:45 -0600

    Add files via upload

---
+++ FlujoArchivos/src/main/MainServidor.java
@@ -0,0 +1,14 @@
+package main;
+import sockets.SRecibe;
+
+public class MainServidor {
+    
+    public static void main(String[] args) {
+        
+        SRecibe s1=new SRecibe();
+        s1.recibeServidor();
+        
+        
+    }
+    
+}
